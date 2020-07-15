0b5dfa827a44f643008642f8bf5b5506fa17c343
Banee Ishaque K <baneeishaque@alkidhmagroup.com>
2020-07-16 00:16:21 +0530

    1.8

--- src/main/java/ndk/banee/jgit_demo/Main.java
+++ src/main/java/ndk/banee/jgit_demo/Main.java
@@ -1,9 +1,15 @@
-package ndk.banee.jgit_demo;;
+package ndk.banee.jgit_demo;
+
+import java.io.File;
+import org.eclipse.jgit.lib.RepositoryCache;
+import org.eclipse.jgit.util.FS;
 
 public class Main {
 
     public static void main(String[] args) {
-        
-        
+
+        File file = new File("E:\\To_Sync\\Lab_Abdulla\\Cordova_Web\\logistic-cordova");
+        System.out.println("Checking for git repository on " + file.getPath());
+        System.out.println(RepositoryCache.FileKey.isGitRepository(file, FS.DETECTED));
     }
 }
